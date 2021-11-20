4349d8c4e15e599b79fda5ed93f6498a34d7d160
SilasAkerman <94759651+SilasAkerman@users.noreply.github.com>
2021-11-20 20:28:18 +0100

    Add files via upload

---
+++ Particle.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Numerics;
+using Raylib_cs;
+
+namespace Perlin_Noise_Field
+{
+    class Particle
+    {
+        Vector2 pos;
+        Vector2 vel;
+        Vector2 acc;
+
+        Color color = new Color(255, 255, 255, 5);
+        Vector2 prevPos;
+        double maxSpeed = 4;
+        int size = 1;
+        Random random = new Random();
+        int hue = 0;
+        
+        public Particle()
+        {
+            pos = new Vector2(random.Next(Program.WIDTH), random.Next(Program.HEIGHT));
+            vel = new Vector2((float)random.NextDouble() * 2 - 1, (float)random.NextDouble() * 2 - 1);//Vector2.Zero;
+            acc = Vector2.Zero;
+            prevPos = new Vector2(pos.X, pos.Y);
+        }
+
+        public void Update(Vector2[] flowfield)
+        {
+            edges();
+            follow(flowfield);
+
+            vel += acc;
+            if (vel.Length() > maxSpeed) vel /= vel.Length() / 4;
+            updatePrevPos();
+            pos += vel;
+            acc *= 0;
+        }
+
+        private void applyForce(Vector2 force)
+        {
+            acc += force;
+        }
+
+        public void Display()
+        {
+            //Raylib.DrawCircleV(pos, size, color);
+            //hue++;
+            //if (hue >= 255) hue = 0;
+            //color = Raylib.ColorFromHSV(hue, 255, 255);
+            //color.a = 15;
+            Raylib.DrawLineEx(pos, prevPos, size, color);
+        }
+
+        private void follow(Vector2[] flowfield)
+        {
+            int row = (int)Math.Floor(pos.Y / Program.SPACING);
+            int col = (int)Math.Floor(pos.X / Program.SPACING);
+            int index = col + row * Program.COLS;
+            applyForce(flowfield[index]);
+        }
+
+        private void edges()
+        {
+            if (pos.X < 0)
+            {
+                pos.X = Program.WIDTH-1;
+                updatePrevPos();
+            }
+            if (pos.X >= Program.WIDTH)
+            {
+                pos.X = 0;
+                updatePrevPos();
+            }
+            if (pos.Y < 0)
+            {
+                pos.Y = Program.HEIGHT-1;
+                updatePrevPos();
+            }
+            if (pos.Y >= Program.HEIGHT)
+            {
+                pos.Y = 0;
+                updatePrevPos();
+            }
+        }
+
+        private void updatePrevPos()
+        {
+            prevPos.X = pos.X;
+            prevPos.Y = pos.Y;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using Raylib_cs;
+using SimplexNoise;
+using System.Numerics;
+
+namespace Perlin_Noise_Field
+{
+    class Program
+    {
+        public const int SPACING = 7;
+        public const int WIDTH = (1400 / SPACING) * SPACING;
+        public const int HEIGHT = (1000 / SPACING) * SPACING;
+
+        public const int ROWS = HEIGHT / SPACING;
+        public const int COLS = WIDTH / SPACING;
+
+        const float noiseScale = 0.005f;
+        static float zOffset = 0;
+        static float zIncrement = 0.2f;
+
+        static Vector2[] flowfield = new Vector2[ROWS * COLS];
+        static Particle[] particles = new Particle[4000];
+
+        static void Main(string[] args)
+        {
+            Noise.Seed = 420;
+            for (int i = 0; i < particles.Length; i++)
+            {
+                particles[i] = new Particle();
+            }
+
+            Raylib.InitWindow(WIDTH, HEIGHT, "Perlin noise Field");
+            Raylib.SetTargetFPS(60);
+            Raylib.BeginDrawing();
+            Raylib.ClearBackground(Color.BLACK);
+            Raylib.EndDrawing();
+
+            while (!Raylib.WindowShouldClose())
+            {
+                Update();
+                Display();
+            }
+
+            Raylib.CloseWindow();
+        }
+
+        static void Update()
+        {
+            setFlowfield();
+            foreach(Particle particle in particles)
+            {
+                particle.Update(flowfield);
+            }
+        }
+
+        static void Display()
+        {
+            Raylib.BeginDrawing();
+            //Raylib.ClearBackground(Color.WHITE);
+            //Raylib.DrawFPS(100, 100);
+            //for (int row = 0; row < ROWS; row++)
+            //{
+            //    for (int col = 0; col < COLS; col++)
+            //    {
+            //        int index = col + row * COLS;
+            //        Vector2 dir = Vector2.Normalize(flowfield[index]) * SPACING;
+            //        Raylib.DrawLine(col * SPACING + SPACING, row * SPACING + SPACING, col * SPACING + (int)dir.X + SPACING, row * SPACING + (int)dir.Y + SPACING, Color.BLACK);
+            //    }
+            //}
+            foreach (Particle particle in particles)
+            {
+                particle.Display();
+            }
+            Raylib.EndDrawing();
+        }
+
+        static void setFlowfield()
+        {
+            zOffset += zIncrement;
+            for (int row = 0; row < ROWS; row++)
+            {
+                for (int col = 0; col < COLS; col++)
+                {
+                    int index = col + row * COLS;
+                    double angle = Noise.CalcPixel3D(col, row, (int)zOffset, noiseScale) / 255 * Math.PI * 2 * 4;
+                    flowfield[index] = new Vector2((float)Math.Sin(angle), (float)Math.Cos(angle)) / 5;
+                }
+            }
+        }
+    }
+}
